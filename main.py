7d40c6611d212f102ed642625da35db2f45e3e35
Manik <manik.219310187@muj.manipal.edu>
2023-06-15 21:27:13 +0530

    making final edits to the main.py which is usable for both student marks as well as student data table

--- main.py
+++ main.py
@@ -23,22 +23,23 @@ def Level2():
     print("Exit: Press 6")
 
 def Level3():
+    print("To insert Student's marks: Press 1")
     print("To Show marks of a Student: Press 2")
     print("To Display Marks of all the Student's: Press 3")
-    print("To Edit marks of a student: Press 4")
+    print("To Edit Subject marks of a student: Press 4")
     print("Exit: Press 6")
  
 choice=0
-Level1
+Level1()
 
 while choice!=6:
-    Level1
+    Level1()
     choice=int(input("Enter your choice:"))
     if choice==1:
         input_1=0
-        Level2()
-
+    
         while input_1!=6:
+            Level2()
             input_1=int(input("Enter your choice: "))
             if input_1==1:
                 RegistrationNumber=int(input("Enter the Student RegistrationNumber"))
@@ -49,23 +50,27 @@ while choice!=6:
             elif input_1==3:
                 Basic_Data.Fetch_All()
             elif input_1==4:
-                while input_1!= 6:
+                while True:
+                    RegistrationNumber = int(input("Enter the Student RegistrationNumber (0 to exit): "))
+                    if RegistrationNumber == 0:
+                        break
                     RegistrationNumber=int(input("Enter the Student RegistrationNumber: "))
                     StudentName=input("Enter the Name of the Student: ")
                     PhoneNumber=int(input("Enter the PhoneNumber number of the Student: "))
                     Basic_Data.insert_user(RegistrationNumber, StudentName, PhoneNumber)
             elif input_1==5:
-                PhoneNumber=int(input())
-                userid=int(input())
-                Basic_Data.Edit_user_PhoneNumber(PhoneNumber, userid)
+                PhoneNumber=int(input("Enter the PhoneNumber number of the Student: "))
+                RegistrationNumber=int(input("Enter the Student RegistrationNumber: "))
+                Basic_Data.Edit_user_PhoneNumber(PhoneNumber, RegistrationNumber)
         
     elif choice==2:
         input_2=0
-        Level3
+        
         while input_2!=6:
+            Level3()
             input_2=int(input("Enter your choice: "))
             if input_2==1:
-                Student_ID=int(input("Enter the Student_ID: "))
+                Student_ID=int(input("Enter the Student RegistrationNumber: "))
                 Maths=int(input("Enter the Maths Marks: "))
                 Chemistry=int(input("Enter the Chemistry Marks: "))
                 Physics=int(input("Enter the Physics Marks: "))
@@ -73,12 +78,12 @@ while choice!=6:
                 Student_Marks.insert_user_marks(Student_ID, Maths, Chemistry, Physics, English)
 
             elif input_2==2:
-                x=int(input("Enter the User_ID"))
+                x=int(input("Enter the Student RegistrationNumber"))
                 Student_Marks.display_user_marks(x)
             elif input_2==3:
                 Student_Marks.Fetch_All()
             elif input_2==4:
-                input_3=int(input("Enter the StudentID: "))
+                input_3=int(input("Enter the Student RegistrationNumber: "))
                 print()
                 print("Maths Marks: Press 1")
                 print("Chemistry Marks: Press 2")
@@ -95,9 +100,3 @@ while choice!=6:
                     Student_Marks.update_physics_marks(input_5, input_3)
                 elif input_4==4:
                     Student_Marks.update_english_marks(input_5, input_3)
-
-
-        # userid=int(input("Enter the user id: "))
-        # StudentName=input("Enter the Name of the user: ")
-        # PhoneNumber=int(input("Enter the PhoneNumber number of the user: "))
-        # Basic_Data.insert_user(userid, StudentName, PhoneNumber)
