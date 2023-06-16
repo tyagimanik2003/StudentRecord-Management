724cee681c5952f03407107763721903dca81a42
Manik Tyagi <94557894+tyagimanik2003@users.noreply.github.com>
2023-06-16 21:22:51 +0530

    Update AttendanceInfo.py

--- AttendanceInfo.py
+++ AttendanceInfo.py
@@ -14,3 +14,18 @@ class StudentAttendance:
         cur=self.con.cursor()
         cur.execute(query)
         self.con.commit()
+    def Mark_present(self,RegistrationNumber,value):
+        query1="Select Present from attendance_record where RegistrationNumber={}".format(RegistrationNumber)
+        cur=self.con.cursor()
+        present=cur.execute(query)+value
+        
+        query2="Update attendance_record set Present ={} where RegistrationNumber={}".format(Present,RegistrationNumber)
+        self.Execute(query2)
+    def Mark_absent(self,RegistrationNumber,value):
+        query1="Select Absent from attendance_record where RegistrationNumber={}".format(RegistrationNumber)
+        cur=self.con.cursor()
+        Absent=cur.execute(query)+value
+        
+        query2="Update attendance_record set Absent ={} where RegistrationNumber={}".format(Absent,RegistrationNumber)
+        self.Execute(query2)
+        
